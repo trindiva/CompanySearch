7c89a2103f6546a50132ae945cd7e06412c61b3f
Ivan Trindev <trindiva@uw.edu>
2020-03-10 10:53:03 -0700

    add in home and work users

--- CompanySearch/CompanySearch.py
+++ CompanySearch/CompanySearch.py
@@ -13,6 +13,11 @@ import decimal
 from win32ctypes.pywin32 import win32api
 import pyautogui
 
+paths = {
+    "home" : "C:\\Users\\seeke\\OneDrive - ACT Capital Advisors\\CompanySearch\\CompanySearch.xlsx",
+    "work" : "C:\\Users\\Ivan Trindev\\ACT Capital Advisors\\CompanySearch\\CompanySearch.xlsx"
+    }
+
 # open chrome and go to zoominfo.com and clickn on the login button
 ### NOT A USED FUNCTION! I AM GOING TO OPEN UP ZOOMINFO MANUALLY AND SET UP ALL THE PARAMS BEFORE RUNNING THE PROGRAM
 def open_chrome():
@@ -42,19 +47,19 @@ def open_chrome():
     time.sleep(float(decimal.Decimal(random.randrange(150, 300))/100))
 
 # get df with company names
-def get_df():
-    path = "C:\\Users\\Ivan Trindev\\ACT Capital Advisors\\DnS - Documents\\General\\D&S Mailing 2.xlsx"
+def get_df(user):
+    path = paths[user]
 
-    df = pd.read_excel(path, sheet_name="Sheet4")
+    df = pd.read_excel(path, sheet_name="Sheet1")
     return df
 
 # search for the companies
-def company_search():
+def company_search(user):
     # sleep for 15 sec to give me time to pull up zoominfo
     time.sleep(5)
 
     # get df
-    df = get_df()
+    df = get_df(user)
 
     # request info for the first 10 companies in the list
     count = 0
@@ -73,4 +78,4 @@ def company_search():
 
         count += 1
 
-company_search()
+company_search("home")
